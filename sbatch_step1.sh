8c9e9beae49028df9324921e1d9b8b43178e30ce
btcarter <benjamin.t.carter@gmail.com>
2019-04-09 11:47:41 -0600

    revising to my liking

--- sbatch_step1.sh
+++ sbatch_step1.sh
@@ -1,54 +1,65 @@
 #!/bin/bash
 
 
-# Written by Nathan Muncy on 11/20/17
+# Originally written by Nathan Muncy on 11/20/17.
+# Then butchered by Ben Carter, 2019-04-09.
 
 
 #SBATCH --time=10:00:00   # walltime
 #SBATCH --ntasks=2   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=32gb   # memory per CPU core
-#SBATCH -J "BenTemp"   # job name
+#SBATCH -J "Struc"   # job name
 
 # Compatibility variables for PBS. Delete if not needed.
 export PBS_NODEFILE=`/fslapps/fslutils/generate_pbs_nodefile`
 export PBS_JOBID=$SLURM_JOB_ID
-export PBS_O_WORKDIR="$SLURM_SUBMIT_DIR"
+export PBS_O_TEMPLATE_DIR="$SLURM_SUBMIT_DIR"
 export PBS_QUEUE=batch
 
 # Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
 export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
 
+#######################
+# --- ENVIRONMENT --- #
+#######################
 
+START_DIR=${pwd} 										# in case you want an easy reference to return to the directory you started in.
+STUDY=~/compute/skilledReading							# location of study directory
+TEMPLATE_DIR=${STUDY}/template 							# destination for template output
+DICOM_DIR=${STUDY}/data/dicomdir 						# location of raw dicoms
+SCRIPT_DIR=${STUDY}/analyses/structuralSkilledReading 	# location of scripts that might be referenced
+LIST=${SCRIPT_DIR}/participants.tsv 					# list of participant IDs
 
-workDir=~/compute/Ben_template
-rawDir=${workDir}/DICOMs
+####################
+# --- COMMANDS --- #
+####################
 
+# Create NIFTI files from the DICOMs
+cd $DICOM_DIR
 
-cd $rawDir
+for i in $(ls $LIST); do
 
-for i in t*; do
+    T1_DIR=${DICOM_DIR}/$i
+    PARTICIPANT_DIR=${TEMPLATE_DIR}/"${i/t1_Luke_Reading_}"
 
-    dataDir=${rawDir}/$i
-    subjDir=${workDir}/"${i/t1_Luke_Reading_}"
-
-    if [ ! -d $subjDir ]; then
-        mkdir $subjDir
+    if [ ! -d $PARTICIPANT_DIR ]; then
+        mkdir $PARTICIPANT_DIR
     fi
 
 
     # construct
-    if [ ! -f ${subjDir}/struct_orig.nii.gz ]; then
+    if [ ! -f ${PARTICIPANT_DIR}/struct_orig.nii.gz ]; then
 
-        cd $dataDir
+        cd $T1_DIR
         dcm2nii -a y -g n -x y *.dcm
-        mv co*.nii ${subjDir}/struct_orig.nii
+        mv co*.nii ${PARTICIPANT_DIR}/struct_orig.nii
         rm *.nii
 
     fi
 
 
-    cd $subjDir
+    cd $PARTICIPANT_DIR
 
     # acpc align
     if [ ! -f struct_acpc.nii.gz ]; then
@@ -77,5 +88,5 @@ for i in t*; do
 
     fi
 
-cd $rawDir
+cd $DICOM_DIR
 done

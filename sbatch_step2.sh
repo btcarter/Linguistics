ef30fa0ffa159cbe467bf12e040cae1d7bd3455e
Benjamin Carter <benjamin.t.carter@gmail.com>
2019-04-10 14:20:21 -0600

    adding my own method to the madness

--- sbatch_step2.sh
+++ sbatch_step2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-# Written by Nathan Muncy on 11/20/17
-
+# Originally written by Nathan Muncy on 11/20/17
+# Butchered by Ben Carter, 2019-04-09.
 
 #SBATCH --time=10:00:00   # walltime
 #SBATCH --ntasks=2   # number of processor cores (i.e. tasks)
@@ -19,18 +19,34 @@ export PBS_QUEUE=batch
 # Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
 export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
 
+#######################
+# --- ENVIRONMENT --- #
+#######################
 
+START_DIR=${pwd} 											# in case you want an easy reference to return to the directory you started in.
+STUDY=~/compute/skilledReadingStudy					    	# location of study directory
+TEMPLATE_DIR=${STUDY}/template 								# destination for template output
+SCRIPT_DIR=~/analyses/structuralSkilledReading				# location of scripts that might be referenced; assumed to be separate from the data directory.
+PARTICIPANT_STRUCT=${STUDY}/structural/${1}					# location of derived participant structural data
+ATLAS=mni_icbm152_t1_tal_nlin_sym_09c.nii				# location of the MNI ICBM 152 atlas files
 
-
-subjDir=$1
-tempDir=$2
+####################
+# --- COMMANDS --- #
+####################
+# ------------------
+# OPERATIONS: these are performed once per participant as submitted.
+# 1. N4BC NIFTIs are subjected to an ANTs based transformation into MNI space.
+#
+# REQUIRES: things needed to run this script
+# 1. MNI aligne ICBM_152 template atlas
+# ------------------
 
 
 cd $subjDir
 
-FIX=${tempDir}/mni_icbm152_t1_tal_nlin_sym_09c.nii
-MOV=struct_n4bc.nii.gz
-OUT=ants_
+
+MOV=${PARTICIPANT_STRUCT}/struct_n4bc.nii.gz
+OUT=${PARTICIPANT_STRUCT}/ants_
 
 ITS=100x100x100x20
 DIM=3
@@ -38,7 +54,7 @@ LMWT=0.9
 INTWT=4
 PCT=0.8
 PARZ=100
-INTENSITY=CC[$FIX,${MOV},${INTWT},4]
+INTENSITY=CC[${ATLAS},${MOV},${INTWT},4]
 
 if [ ! -f ${OUT}Affine.txt ]; then
 
@@ -53,8 +69,8 @@ if [ ! -f ${OUT}Affine.txt ]; then
 fi
 
 
-if [ ! -f struct_mni.nii.gz ]; then
+if [ ! -f ${PARTICIPANT_STRUCT}/struct_mni.nii.gz ]; then
 
-    WarpImageMultiTransform $DIM $MOV struct_mni.nii.gz ${OUT}Warp.nii.gz ${OUT}Affine.txt -R $FIX
+    WarpImageMultiTransform $DIM $MOV struct_mni.nii.gz ${OUT}Warp.nii.gz ${OUT}Affine.txt -R ${ATLAS}
 
 fi

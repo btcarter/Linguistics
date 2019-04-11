e8553611b3f819540048426654c9c0a1b343d5bc
Benjamin Carter <benjamin.t.carter@gmail.com>
2019-04-11 12:43:33 -0600

    updating submission script so everything points in the right place.

--- submit_sbatch_step2.sh
+++ submit_sbatch_step2.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 
 
-# Written by Nathan Muncy on 11/20/17
+# Originally written by Nathan Muncy on 11/20/17.
+# Butchered by Ben Carter, 2019-04-09.
 
+# This script will check for the necessary directory structure and kill itself when things go awry or submit 1 job per participant.
 
-workDir=~/compute/Ben_template
-slurmDir=${workDir}/Slurm_out
-tempDir=~/bin/Templates/old_templates/mni_icbm152_nlin_sym_09c_nifti/mni_icbm152_nlin_sym_09c
+#####################
+# --- VARIABLES --- #
+#####################
 
-time=`date '+%Y_%m_%d-%H_%M_%S'`
-outDir=${slurmDir}/template_${time}
+STUDY=~/compute/skilledReadingStudy					    # location of study directory
+TEMPLATE_DIR=${STUDY}/template 							# destination for template output
+SCRIPT_DIR=~/analyses/structuralSkilledReading			# location of scripts that might be referenced; assumed to be separate from the data directory.
+LIST=${SCRIPT_DIR}/participants.tsv 					# list of participant IDs
+LOG=~/logfiles											# where to put documentation about errors and outputs
+TIME=`date '+%Y_%m_%d-%H_%M_%S'`						# time stamp for e's and o's
 
-cd $slurmDir
-if [ ! -d $outDir ]; then
-    mkdir $outDir
+# check for participant list
+if [ ! -f ${LIST} ]; then
+	echo ${LIST} does not exist, check your variable LIST
+	exit 1
 fi
 
+# check for logfiles destination
+OUT=${LOG}/TEMPLATE_STEP2_${TIME}
+if [ ! -d ${OUT} ]; then
+	mkdir -p ${OUT}
+fi
 
-cd $workDir
-
-for i in S* ; do
-
-    subjDir=${workDir}/${i}
-
+# submit the job script once per participant
+for i in $(cat $LIST); do
+	
     sbatch \
-    -o ${outDir}/output_step2_${i}.txt \
-    -e ${outDir}/error_step2_${i}.txt \
-    sbatch_step2.sh $subjDir $tempDir
+    -o ${OUT}/output_step2_${i}.txt \
+    -e ${OUT}/error_step2_${i}.txt \
+    ${SCRIPT_DIR}/sbatch_step2.sh ${i}
 
     sleep 1
 

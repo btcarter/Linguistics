1b521bd45d15751d30f6928b86ccfe1363672c1e
Benjamin Carter <benjamin.t.carter@gmail.com>
2019-04-11 14:30:35 -0600

    fixing an error made in paste!

--- submit_sbatch_step3.sh
+++ submit_sbatch_step3.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-#SBATCH --time=10:00:00   # walltime
-#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
-#SBATCH --nodes=1   # number of nodes
-#SBATCH --mem-per-cpu=32gb   # memory per CPU core
-#SBATCH -J "step3"   # job name
-#SBATCH --mail-user=ben88@byu.edu  # email address
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
-
-# Compatibility variables for PBS. Delete if not needed.
-export PBS_NODEFILE=`/fslapps/fslutils/generate_pbs_nodefile`
-export PBS_JOBID=$SLURM_JOB_ID
-export PBS_O_TEMPLATE_DIR="$SLURM_SUBMIT_DIR"
-export PBS_QUEUE=batch
-
-# Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
-export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
-
 # Written by Ben Carter, 2019-04-11.
 
 #####################

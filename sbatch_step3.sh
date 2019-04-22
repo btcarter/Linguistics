3f1ff663d4ebd346db0334a3a7bdd1f5ec5b9f9e
btcarter <benjamin.t.carter@gmail.com>
2019-04-22 15:20:31 -0600

    backing off on processors

--- sbatch_step3.sh
+++ sbatch_step3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --time=96:00:00   # walltime
-#SBATCH --ntasks=6   # number of processor cores (i.e. tasks)
+#SBATCH --ntasks=2   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=65536M   # memory per CPU core
 #SBATCH -J "step3"   # job name
